c2ad1f037c1a6911389e5e0fbc41854c83217b26
traorecheikh <atidiane741@gmail.com>
2025-07-18 18:04:06 +0000

    Add IdentiteMapperImpl and IdentiteRepository for user data mapping and persistence

---
+++ src/main/java/com/quantisen/boisson/application/identite/mappers/IdentiteMapperImpl.java
@@ -0,0 +1,64 @@
+package com.quantisen.boisson.application.identite.mappers;
+
+import com.quantisen.boisson.application.identite.dtos.IdentiteDto;
+import com.quantisen.boisson.domaine.identite.domainModel.CompteUtilisateur;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import java.util.List;
+
+@ApplicationScoped
+@Named
+public class IdentiteMapperImpl implements IdentiteMapper {
+    public IdentiteDto toDto(CompteUtilisateur utilisateur) {
+        if (utilisateur == null) return null;
+        IdentiteDto dto = new IdentiteDto();
+        dto.setId(utilisateur.getId());
+        dto.setEmail(utilisateur.getEmail());
+        dto.setRole(utilisateur.getRole());
+        dto.setCreatedAt(utilisateur.getCreatedAt());
+        dto.setUpdatedAt(utilisateur.getUpdatedAt());
+        dto.setActive(utilisateur.isActive());
+        return dto;
+    }
+
+    public CompteUtilisateur toEntity(IdentiteDto dto) {
+        return toEntity(dto, false);
+    }
+
+    @Override
+    public CompteUtilisateur toEntity(IdentiteDto dto, boolean includePassword) {
+        if (dto == null) return null;
+
+        CompteUtilisateur utilisateur = new CompteUtilisateur();
+        utilisateur.setId(dto.getId());
+        utilisateur.setEmail(dto.getEmail());
+        utilisateur.setRole(dto.getRole());
+        utilisateur.setActive(dto.isActive());
+        utilisateur.setCreatedAt(dto.getCreatedAt());
+        utilisateur.setUpdatedAt(dto.getUpdatedAt());
+
+        if (includePassword) {
+            utilisateur.setMotDePasse(dto.getMotDePasse());
+        }
+
+        return utilisateur;
+    }
+
+
+    @Override
+    public List<IdentiteDto> toDtoList(List<CompteUtilisateur> utilisateurList) {
+        if (utilisateurList == null || utilisateurList.isEmpty()) return List.of();
+        return utilisateurList.stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    @Override
+    public List<CompteUtilisateur> toEntityList(List<IdentiteDto> utilisateurDtos) {
+        if (utilisateurDtos == null || utilisateurDtos.isEmpty()) return List.of();
+        return utilisateurDtos.stream()
+                .map(this::toEntity)
+                .toList();
+    }
+}
---
+++ src/main/java/com/quantisen/boisson/infrastructure/persistence/identite/IdentiteRepository.java
@@ -0,0 +1,143 @@
+package com.quantisen.boisson.infrastructure.persistence.identite;
+
+import com.quantisen.boisson.domaine.identite.domainModel.CompteUtilisateur;
+import com.quantisen.boisson.domaine.identite.port.IdentitePort;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.LockModeType;
+
+import java.util.List;
+
+@RequestScoped
+@Named
+public class IdentiteRepository implements IdentitePort {
+
+    @Inject
+    private EntityManager entityManager;
+
+    @Override
+    public CompteUtilisateur getByEmail(String email) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            if (!tx.isActive()) {
+                tx.begin();
+            }
+            entityManager.flush();
+            return entityManager.createQuery("SELECT u FROM CompteUtilisateur u WHERE u.email = :email", CompteUtilisateur.class)
+                    .setParameter("email", email)
+                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+                    .getSingleResultOrNull();
+        } catch (Exception e) {
+            System.err.println("Error retrieving user by email: " + e.getMessage());
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Error retrieving user by email: " + e.getMessage(), e);
+
+        }
+    }
+
+    @Override
+    public CompteUtilisateur save(CompteUtilisateur utilisateur) {
+
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            if (!tx.isActive()) {
+                tx.begin();
+            }
+            entityManager.persist(utilisateur
+            );
+            entityManager.merge(utilisateur);
+            entityManager.flush();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+        return utilisateur;
+    }
+
+    @Override
+    public CompteUtilisateur getById(Long id) {
+        CompteUtilisateur utilisateur = entityManager.find(CompteUtilisateur.class, id);
+        if (utilisateur == null) {
+            System.err.println("User with id " + id + " not found.");
+            throw new RuntimeException("User not found with id: " + id);
+        } else {
+            System.out.println("User found: " + utilisateur);
+            return utilisateur;
+        }
+    }
+
+    @Override
+    public List<CompteUtilisateur> saveAll(List<CompteUtilisateur> utilisateurs) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            if (!tx.isActive()) {
+                tx.begin();
+            }
+            for (CompteUtilisateur utilisateur : utilisateurs) {
+                entityManager.merge(utilisateur);
+            }
+            entityManager.flush();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Error saving users: " + e.getMessage(), e);
+        }
+        return utilisateurs;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try {
+            entityManager.getTransaction().begin();
+            CompteUtilisateur utilisateur = entityManager.find(CompteUtilisateur.class, id);
+            if (utilisateur != null) {
+                entityManager.remove(utilisateur);
+                entityManager.getTransaction().commit();
+                return true;
+            } else {
+                System.err.println("User with id " + id + " not found.");
+                if (entityManager.getTransaction().isActive()) {
+                    entityManager.getTransaction().rollback();
+                }
+                return false;
+            }
+        } catch (Exception e) {
+            System.err.println("Error deleting user: " + e.getMessage());
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public List<CompteUtilisateur> findAll() {
+        return entityManager.createQuery("SELECT u FROM CompteUtilisateur u", CompteUtilisateur.class)
+                .getResultList();
+    }
+
+    @Override
+    public CompteUtilisateur findById(Long id) {
+        CompteUtilisateur utilisateur = getById(id);
+        if (utilisateur == null) {
+            System.err.println("User with id " + id + " not found.");
+            throw new RuntimeException("User not found with id: " + id);
+        } else {
+            System.out.println("User found: " + utilisateur);
+            return utilisateur;
+        }
+    }
+
+}
+
+
