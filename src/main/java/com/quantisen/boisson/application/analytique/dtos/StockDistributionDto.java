dcc94579e963eaac3cf9ae17a83b1a594f6bae5a
traorecheikh <atidiane741@gmail.com>
2025-07-18 18:07:28 +0000

    Add StockDistributionDto and StockDto for stock management and analytics

---
+++ src/main/java/com/quantisen/boisson/application/analytique/dtos/StockDistributionDto.java
@@ -0,0 +1,17 @@
+package com.quantisen.boisson.application.analytique.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StockDistributionDto {
+    private String category;
+    private double percentage;
+    private double value;
+    private String color;
+}
---
+++ src/main/java/com/quantisen/boisson/application/stockage/dtos/StockDto.java
@@ -0,0 +1,25 @@
+package com.quantisen.boisson.application.stockage.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StockDto {
+    @JsonProperty("nom")
+    private String nom;
+    @JsonProperty("quantiteActuelle")
+    private int quantiteActuelle;
+    @JsonProperty("seuil")
+    private String seuil;
+    @JsonProperty("status")
+    private String status;
+
+}
