a4dce49bccb6c4981f4dfd901dfb8d7b14b7e3cf
traorecheikh <atidiane741@gmail.com>
2025-07-18 17:38:05 +0000

    Add BoissonMapper interface for entity and DTO conversion

---
+++ src/main/java/com/quantisen/boisson/application/boisson/mappers/BoissonMapper.java
@@ -0,0 +1,16 @@
+package com.quantisen.boisson.application.boisson.mappers;
+
+import com.quantisen.boisson.application.boisson.dtos.BoissonDto;
+import com.quantisen.boisson.domaine.boisson.domainModel.Boisson;
+
+import java.util.List;
+
+public interface BoissonMapper {
+    Boisson toEntity(BoissonDto boissonDto);
+
+    BoissonDto toDto(Boisson boisson);
+
+    List<BoissonDto> toDtoList(List<Boisson> boissons);
+
+    List<Boisson> toEntityList(List<BoissonDto> boissonDtos);
+}
