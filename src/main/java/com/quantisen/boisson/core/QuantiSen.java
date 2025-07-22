6f219419b85b0501c4b92891fa8f985cc9a8d9b3
traorecheikh <atidiane741@gmail.com>
2025-07-22 22:58:09 +0000

    Update Logback dependencies and add Loki appender; log application startup in QuantiSen

--- src/main/java/com/quantisen/boisson/core/QuantiSen.java
+++ src/main/java/com/quantisen/boisson/core/QuantiSen.java
@@ -1,8 +1,18 @@
 package com.quantisen.boisson.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class QuantiSen extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(QuantiSen.class);
+    public QuantiSen() {
+        logger.info("QuantiSen application started");
+//        // Send 500 logs to Loki
+//        for (int i = 1; i <= 50; i++) {
+//            logger.info("Loki test log #{}", i);
+//        }
+    }
 }
